022d96c5d86f5da7950814045c7fdafa8321731d
Danae Dunlap <danae.736256@nv.ccsd.net>
2020-09-07 01:59:00 +0000

    Fixed StudentDesks

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -10,19 +10,34 @@ class StudentDesks {
         int classOne = scanner.nextInt(); 
 				int classTwo = scanner.nextInt(); 
 				int classThree = scanner.nextInt(); 
+				int deskOne, deskTwo, deskThree; 
 
         /*
          *  your code goes here
          */
-				int totalKids = classOne + classTwo + classThree; 
-				int totalDesk;
 
-				if (totalKids % 2 == 0){
-					totalDesk = totalKids / 2; 
+				if (classOne % 2 == 0){
+						deskOne = classOne / 2; 
 				} 
 				else {
-					totalDesk = (totalKids / 2) + 1; 
+					deskOne = (classOne / 2) + 1; 
 				}
+
+				if(classTwo % 2 == 0){
+					deskTwo = classTwo / 2; 
+				}
+				else {
+					deskTwo = (classTwo / 2) + 1; 
+				}
+
+				if(classThree % 2 == 0){
+					deskThree = classThree / 2; 
+				}
+				else {
+					deskThree = (classThree / 2) + 1; 
+				}
+
+				int totalDesk = deskOne + deskTwo + deskThree; 
 
 				System.out.print(totalDesk);
 
